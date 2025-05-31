1c68440f7a8ae418a7249fb18c3fd466eea610a4
gerusura <gerusuraimu@mail.gerusuraimu.com>
2025-05-31 12:45:56 +0900

    Deploy commit

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ from utils import (
 server = get_server()
 
 
-@server.post('/auth')
+@server.post('/publish/auth')
 async def auth(payload: AuthPayload):
     return request(payload, 'auth')
 
 
-@server.post('/login')
+@server.post('/publish/login')
 async def login(payload: LoginPayload):
     return request(payload, 'login')
 
 
-@server.post('/logout')
+@server.post('/publish/logout')
 async def logout(payload: LogoutPayload):
     return request(payload, 'logout')
 
 
-@server.post('/register')
+@server.post('/publish/register')
 async def register(payload: RegisterPayload):
     return request(payload, 'register')
 
 
-@server.post('/embed')
+@server.post('/publish/embed')
 async def embed(payload: AuthPayload):
     return request(payload, 'embed')
